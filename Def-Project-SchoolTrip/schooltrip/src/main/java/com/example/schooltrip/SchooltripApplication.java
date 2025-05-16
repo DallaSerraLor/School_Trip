46d2e60378e49368577c3cb2b789ad4310f70ace
Utente <Utente@DESKTOP-KBGCENL>
2025-05-16 17:30:00 +0200

    a

---
+++ Def-Project-SchoolTrip/schooltrip/src/main/java/com/example/schooltrip/SchooltripApplication.java
@@ -0,0 +1,13 @@
+package com.example.schooltrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooltripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooltripApplication.class, args);
+	}
+
+}
--- HelloWorld-ChatGPT/src/main/java/com/example/helloworld/controller/MessageController.java
+++ HelloWorld-ChatGPT/src/main/java/com/example/helloworld/controller/MessageController.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/message")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class MessageController {
-
     @Autowired
     private MessageService messageService;
 
-    @GetMapping
+    @RequestMapping("/api/message")
     public Message getMessage() {
         return messageService.getHelloMessage();
     }
+    
+    @RequestMapping("/api/m2")
+    public String getMessage2() {
+        return "hello";
+    }
+
 }
